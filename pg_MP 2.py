476ff14b8c6aa2a3d064cf74c3bca3ddf9350a7a
MackenzieP30 <33524219+MackenzieP30@users.noreply.github.com>
2018-01-11 12:30:57 -0500

    Add files via upload

---
+++ pg_MP 2.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+import time
+
+pg.hotkey ('ctrl' , 'winleft' , 'd')
+pg.hotkey('winleft')
+pg.typewrite('chrome\n',.3)
+pg.hotkey('winleft','up')
+pg.typewrite('youtube.com\n')
+pg.typewrite('cute cat videos\n',.3)
+
+
+
+
